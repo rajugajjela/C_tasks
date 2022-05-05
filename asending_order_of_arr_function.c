592c974569749f6985b94df18838a046dd732693
rajugajjela <104496293+rajugajjela@users.noreply.github.com>
2022-05-05 10:49:30 +0530

    first commit

---
+++ asending_order_of_arr_function.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int asending(int *arr, int size)
+{
+    int temp;
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = i + 1; j < size; j++)
+        {
+            if (arr[i] > arr[j])
+            {
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    for (int i = 0; i < size; i++)
+        printf("after sort arr[%d]:%d\n", i, arr[i]);
+}
+int main()
+{
+    int i, size; // sorting array in asending order
+    int arr[10] = {2, 4, 1, 55, 43, 22, 23, 19, 11, 3};
+    size = sizeof(arr) / sizeof(int);
+    for (int i = 0; i < size; i++)
+        printf("before sort arr[%d]:%d\n", i, arr[i]);
+    asending(arr, size);
+}
